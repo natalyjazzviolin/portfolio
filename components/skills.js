a42bbc5a1908b84cc1dd5170b3e1f83e82c15050
Nataly Merezhuk <hello@nataly.dev>
2022-02-14 22:13:40 -0500

    Removed Skills title.

--- components/skills.js
+++ components/skills.js
@@ -3,7 +3,7 @@ import "../styles/About.module.scss";
 export default function Skills() {
   return (
     <section className="container">
-      <h3 className="container__title">Skills</h3>
+      {/* <h3 className="container__title">Skills</h3> */}
       <div className="container__skills">
           skills go here
       </div>

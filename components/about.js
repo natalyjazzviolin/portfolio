06522eb7bba3a2e95d18653b6902fe760443b443
Nataly Merezhuk <hello@nataly.dev>
2022-03-09 20:20:09 -0500

    Skeleton for about photo with margins.

--- components/about.js
+++ components/about.js
@@ -1,24 +1,64 @@
 import "../styles/About.module.scss"
 import avatar from "../public/avatar.png"
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function About() {
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
+
     return (
       <section className="about">
-        <div className="about__container">
-          <Image
-            className="about__container-photo"
-            src={avatar}
-            alt="Portrait"
-            width={300}
-            height={300}
-            layout="fixed"
-            priority={true}
-            // placeholder="blur"
-          />
-        </div>
+        {loading ? (
+          <SkeletonTheme
+            highlightColor="rgb(220, 215, 198)"
+            baseColor="rgb(232, 228, 212)"
+          >
+            <Skeleton
+              className="about__skeleton"
+              circle={true}
+              height={300}
+              width={300}
+            />
+          </SkeletonTheme>
+        ) : (
+          <div className="about__container">
+            <Image
+              className="about__container-photo"
+              src={avatar}
+              alt="Portrait"
+              width={300}
+              height={300}
+              layout="fixed"
+              priority={true}
+              onLoadingComplete={() => setLoading(false)}
+            />
+          </div>
+        )}
         <div className="about__bio">
-          <p> Nataly is a jazz violinist and self-taught fullstack developer. She is currently working with React, Laravel, and Python at <span className="highlight"><a target="_blank" href="https://www.wizardpig.com" rel="noreferrer">WizardPig Inc.</a></span></p>
+          <p>
+            {" "}
+            Nataly is a jazz violinist and self-taught fullstack developer. She
+            is currently working with React, Laravel, and Python at{" "}
+            <span className="highlight">
+              <a
+                target="_blank"
+                href="https://www.wizardpig.com"
+                rel="noreferrer"
+              >
+                WizardPig Inc.
+              </a>
+            </span>
+          </p>
         </div>
       </section>
     );
